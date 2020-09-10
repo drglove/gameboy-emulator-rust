4ea7193e7f9512d2eabe0233a1b781941069da40
Nicholas Lange <nick.t.lange@gmail.com>
2020-09-09 20:57:49 -0700

    Add a FrameSequencer that will can tick down and notify when its time to run something, and will then be reset.

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -6,6 +6,7 @@ pub mod cpal_audio_output;
 pub struct APU {
     square_with_sweep: SquareChannel,
     square_without_sweep: SquareChannel,
+    frame_sequencer: FrameSequencer,
 }
 
 pub trait AudioPlayer {
@@ -13,17 +14,27 @@ pub trait AudioPlayer {
     fn sample_rate(&self) -> u32;
 }
 
+const MASTER_FRAME_SEQUENCER_CLOCK_RATE_HZ: u32 = 512;
+const MASTER_FRAME_SEQUENCER_CLOCKS: u32 =
+    crate::cpu::CPU_CLOCK_RATE_HZ / MASTER_FRAME_SEQUENCER_CLOCK_RATE_HZ;
+
 impl APU {
     pub fn new() -> Self {
         APU {
             square_with_sweep: SquareChannel::new_with_sweep(),
             square_without_sweep: SquareChannel::new_without_sweep(),
+            frame_sequencer: FrameSequencer {
+                timer: 0, // We want the first tick to fire
+                initial: MASTER_FRAME_SEQUENCER_CLOCKS,
+            },
         }
     }
 
     pub fn initialize_buffers(&mut self, sample_rate: u32, clock_rate: u32) {
-        self.square_with_sweep.initialize_buffer(sample_rate, clock_rate);
-        self.square_without_sweep.initialize_buffer(sample_rate, clock_rate);
+        self.square_with_sweep
+            .initialize_buffer(sample_rate, clock_rate);
+        self.square_without_sweep
+            .initialize_buffer(sample_rate, clock_rate);
     }
 
     pub fn step(&mut self, cycles: u8) {
@@ -72,5 +83,22 @@ impl APU {
             0xFF14 => NoiseRegister::write_nr14(value, &mut self.square_with_sweep),
             _ => panic!("Unknown command when writing to APU IO register!"),
         }
+    }
+}
+
+struct FrameSequencer {
+    timer: u32,
+    initial: u32,
+}
+
+impl FrameSequencer {
+    fn step(&mut self) -> bool {
+        let (decremented_timer, underflow) = self.timer.overflowing_sub(1);
+        self.timer = if underflow {
+            self.initial
+        } else {
+            decremented_timer
+        };
+        underflow
     }
 }
